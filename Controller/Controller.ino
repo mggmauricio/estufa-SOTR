9977495d20b3297c389a85f336c57ba939829007
Maurício Godoy <mauriciomgodoy01@gmail.com>
2023-12-14 14:42:45 -0300

    feat: Tasks created

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,51 +1,90 @@
 #include <TaskScheduler.h>
+#include<SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+
 
 Scheduler scheduler;
 
-float temperaturaRecebida;  // Variável global para armazenar a temperatura recebida
+  // Variável global para armazenar a temperatura recebida
 float pwmCalculado;        // Variável global para armazenar o PWM calculado
+float x1 = 0.0;
+float b0 = 0.10243; 
+float b1 = 0.005848753; 
+float b2 = -0.096581247;
+float a1 = 0.94186, a2 = -0.0574;
+float setpoint = 30.0;
+static float ek1 = 0, ek2 = 0;
+static float uk1 = 0, uk2 = 0;
+
+SoftwareSerial hc12(10,11);
+
+LiquidCrystal_I2C lcd(0x38,16,2);
+
 
 void receiveDataTask() {
-  // Receba os dados do primeiro microcontrolador por comunicação sem fio
-  // temperaturaRecebida = receberDadosDoMicrocontrolador1();
+  if (hc12.available() > 0){
+    x1 = hc12.parseFloat();
+  } 
 }
 
+
 void calculatePWMTask() {
-  // Realize o cálculo do PWM com base na temperatura recebida
-  // pwmCalculado = calcularPWM(temperaturaRecebida);
+  float ek0 = setpoint - x1;
+  float uk0 = a1*uk1 + a2*uk2 + b0*ek0 + b1*ek1 + b2*ek2;
+  uk2 = uk1;
+  uk1 = uk0;
+  ek2 = ek1;
+  ek1 = ek0;
+  float y1 = uk0;
+  pwmCalculado = map(y1*100, -7, 18, 0, 255);
+  if (pwmCalculado >= 255){
+    pwmCalculado = 255;
+  }
+  else if(pwmCalculado <= 0){
+    pwmCalculado = 0;
+
+  }
 }
 
 void sendPWMTask() {
+  hc12.print(pwmCalculado);
+
+  Serial.print("PWM Enviado");
+  Serial.println(pwmCalculado);
   // Envie o PWM calculado para o primeiro microcontrolador por comunicação sem fio
   // enviarPWMParaMicrocontrolador1(pwmCalculado);
 }
 
 void updateDisplayTask() {
-  // Atualize o display com os dados recebidos e o PWM calculado
-  // atualizarDisplay(temperaturaRecebida, pwmCalculado);
+  lcd.clear();             
+  lcd.print("Temperatura");                 
+  lcd.setCursor(13,0);
+  lcd.print(round(x1));
+  lcd.setCursor(15,0);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("SetPoint");
+  lcd.setCursor(13,1);
+  lcd.print(setpoint);
+  lcd.setCursor(15,1);
+  lcd.print("C");
 }
 
-Task receiveData(1000, TASK_FOREVER, &receiveDataTask);
-Task calculatePWM(1000, TASK_FOREVER, &calculatePWMTask);
-Task sendPWM(1000, TASK_FOREVER, &sendPWMTask);
-Task display(1000, TASK_FOREVER, &updateDisplayTask);
+Task receiveData(10, TASK_FOREVER, &receiveDataTask);
+Task calculatePWM(20, TASK_FOREVER, &calculatePWMTask);
+Task sendPWM(21, TASK_FOREVER, &sendPWMTask);
+Task display(50, TASK_FOREVER, &updateDisplayTask);
 
 void setup() {
-  // Inicialize o módulo de comunicação sem fio, display, etc.
-  // inicializarComunicacaoSemFio();
-  // inicializarDisplay();
 
   scheduler.init();
   scheduler.addTask(receiveData);
   scheduler.addTask(calculatePWM);
   scheduler.addTask(sendPWM);
   scheduler.addTask(display);
-
-  // scheduler.setHighPriority(1, receiveDataTask);
-  // scheduler.setMediumPriority(2, calculatePWMTask);
-  // scheduler.setMediumPriority(3, sendPWMTask);
-  // scheduler.setLowPriority(4, displayTask);
-
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
   scheduler.startNow();
 }
 
--- g_s/g_s.ino
+++ g_s/g_s.ino
@@ -1,42 +1,41 @@
 #include <TaskScheduler.h>  // Substitua com a biblioteca RF real que você está usando
+#include <SoftwareSerial.h>
 
+
+SoftwareSerial hc12(10,11);
 Scheduler scheduler;
 
 float temperaturaAtual;  // Variável global para armazenar a temperatura medida
 float pwmRecebido;      // Variável global para armazenar o PWM recebido do segundo microcontrolador
 
 void readTemperatureTask() {
-  // // Implemente a leitura do sensor de temperatura
-  // temperaturaAtual = lerSensorTemperatura();
+  temperaturaAtual = analogRead(A0);
 }
 
 void sendTemperatureTask() {
-  // // Envie os dados para o segundo microcontrolador por comunicação sem fio
-  // enviarDadosParaMicrocontrolador2(temperaturaAtual);
+  hc12.print(temperaturaAtual);
 }
 
 void executeReceivedPWMTask() {
-  // // Execute a lógica com base no PWM recebido do segundo microcontrolador
-  // executarLogicaComPWM(pwmRecebido);
+  analogWrite(3, pwmRecebido);
 }
 
-Task readTask(1000, TASK_FOREVER, &readTemperatureTask);
-Task sendTask(1000, TASK_FOREVER, &sendTemperatureTask);
-Task executePWM(1000, TASK_FOREVER, &executeReceivedPWMTask);
+void receivePWM(){
+  pwmRecebido = hc12.parseFloat();
+}
+
+Task readTask(5, TASK_FOREVER, &readTemperatureTask);
+Task sendTask(10, TASK_FOREVER, &sendTemperatureTask);
+Task executePWM(20, TASK_FOREVER, &executeReceivedPWMTask);
 
 void setup() {
-  // Inicialize o sensor de temperatura, módulo de comunicação sem fio, etc.
-  // inicializarSensorTemperatura();
-  // inicializarComunicacaoSemFio();
 
+  hc12.begin(9600);
   scheduler.init();
   scheduler.addTask(readTask);
   scheduler.addTask(sendTask);
   scheduler.addTask(executePWM);
-  
-  // scheduler.setHighPriority(1, readTask);
-  // scheduler.setLowPriority(2, sendTask);
-  // scheduler.setLowPriority(3, executePWMTask);
+
 
   scheduler.startNow();
 }
