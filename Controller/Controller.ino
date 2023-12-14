db2922b0390fc8b48221d77a421487ee8f013c02
Maurício Godoy <mauriciomgodoy01@gmail.com>
2023-12-13 21:19:26 -0300

    Change code to work with two microcontrolers

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,28 +1,51 @@
 #include <TaskScheduler.h>
 
-void receiveDataTaskCallback() {
-  // Implemente a lógica de recebimento de dados do Microcontrolador 1
-}
-
-void controlTaskCallback() {
-  // Implemente a lógica do controle, receba o dado do Microcontrolador 1, execute o controlador e envie o PWM para o Microcontrolador 3
-}
-
-void pwmSendTaskCallback() {
-  // Implemente a lógica de envio do PWM para o Microcontrolador 3
-}
-
 Scheduler scheduler;
 
-Task receiveDataTask(100, TASK_FOREVER, &receiveDataTaskCallback);  // Tarefa de leitura do sensor
-Task controlTask(200, TASK_FOREVER, &controlTaskCallback);          // Tarefa de controle
-Task pwmSendTask(300, TASK_FOREVER, &pwmSendTaskCallback);          // Tarefa de envio do PWM
+float temperaturaRecebida;  // Variável global para armazenar a temperatura recebida
+float pwmCalculado;        // Variável global para armazenar o PWM calculado
+
+void receiveDataTask() {
+  // Receba os dados do primeiro microcontrolador por comunicação sem fio
+  // temperaturaRecebida = receberDadosDoMicrocontrolador1();
+}
+
+void calculatePWMTask() {
+  // Realize o cálculo do PWM com base na temperatura recebida
+  // pwmCalculado = calcularPWM(temperaturaRecebida);
+}
+
+void sendPWMTask() {
+  // Envie o PWM calculado para o primeiro microcontrolador por comunicação sem fio
+  // enviarPWMParaMicrocontrolador1(pwmCalculado);
+}
+
+void updateDisplayTask() {
+  // Atualize o display com os dados recebidos e o PWM calculado
+  // atualizarDisplay(temperaturaRecebida, pwmCalculado);
+}
+
+Task receiveData(1000, TASK_FOREVER, &receiveDataTask);
+Task calculatePWM(1000, TASK_FOREVER, &calculatePWMTask);
+Task sendPWM(1000, TASK_FOREVER, &sendPWMTask);
+Task display(1000, TASK_FOREVER, &updateDisplayTask);
 
 void setup() {
+  // Inicialize o módulo de comunicação sem fio, display, etc.
+  // inicializarComunicacaoSemFio();
+  // inicializarDisplay();
+
   scheduler.init();
-  scheduler.addTask(receiveDataTask);
-  scheduler.addTask(controlTask);
-  scheduler.addTask(pwmSendTask);
+  scheduler.addTask(receiveData);
+  scheduler.addTask(calculatePWM);
+  scheduler.addTask(sendPWM);
+  scheduler.addTask(display);
+
+  // scheduler.setHighPriority(1, receiveDataTask);
+  // scheduler.setMediumPriority(2, calculatePWMTask);
+  // scheduler.setMediumPriority(3, sendPWMTask);
+  // scheduler.setLowPriority(4, displayTask);
+
   scheduler.startNow();
 }
 
--- g_s/g_s.ino
+++ g_s/g_s.ino
@@ -1,28 +1,46 @@
-#include <TaskScheduler.h>
-// #include <VirtualWire.h>
+#include <TaskScheduler.h>  // Substitua com a biblioteca RF real que você está usando
 
-// Crie um objeto Scheduler
-
-
-void pwmReceiveTaskCallback() {
-  // Implemente a lógica de recepção do PWM enviado pelo Microcontrolador 2
-}
-
-// Defina as tarefas
-Task pwmReceiveTask(100, TASK_FOREVER, &pwmReceiveTaskCallback);
 Scheduler scheduler;
 
+float temperaturaAtual;  // Variável global para armazenar a temperatura medida
+float pwmRecebido;      // Variável global para armazenar o PWM recebido do segundo microcontrolador
+
+void readTemperatureTask() {
+  // // Implemente a leitura do sensor de temperatura
+  // temperaturaAtual = lerSensorTemperatura();
+}
+
+void sendTemperatureTask() {
+  // // Envie os dados para o segundo microcontrolador por comunicação sem fio
+  // enviarDadosParaMicrocontrolador2(temperaturaAtual);
+}
+
+void executeReceivedPWMTask() {
+  // // Execute a lógica com base no PWM recebido do segundo microcontrolador
+  // executarLogicaComPWM(pwmRecebido);
+}
+
+Task readTask(1000, TASK_FOREVER, &readTemperatureTask);
+Task sendTask(1000, TASK_FOREVER, &sendTemperatureTask);
+Task executePWM(1000, TASK_FOREVER, &executeReceivedPWMTask);
+
 void setup() {
-  // Inicialize o objeto Scheduler
+  // Inicialize o sensor de temperatura, módulo de comunicação sem fio, etc.
+  // inicializarSensorTemperatura();
+  // inicializarComunicacaoSemFio();
+
   scheduler.init();
-  // Adicione as tarefas ao Scheduler
-  scheduler.addTask(pwmReceiveTask);
-  // Inicie o Scheduler
+  scheduler.addTask(readTask);
+  scheduler.addTask(sendTask);
+  scheduler.addTask(executePWM);
+  
+  // scheduler.setHighPriority(1, readTask);
+  // scheduler.setLowPriority(2, sendTask);
+  // scheduler.setLowPriority(3, executePWMTask);
+
   scheduler.startNow();
 }
 
 void loop() {
-  // Execute o Scheduler
   scheduler.execute();
 }
-
